20e13d09e496fb79a255d7e1ace97e3eb5bf4349
Elana Ryznar <eryzna@gmail.com>
2020-03-16 05:33:30 +0000

    Automatically backed up by Learn

--- db/migrate/20200316053134_add_columns_to_preferences.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsToPreferences < ActiveRecord::Migration[5.0]
-  def change
-    add_column :preferences do |t|
-      t.boolean :allow_create_artists
-      t.boolean :allow_create_songs
-    end
-  end
-end
